f7e1b1ea4d9171e51c6164b6ce0828675de315d1
Anson-Mah-ACITECH <mah9689@student.acitech.org>
2024-04-28 02:31:34 -0400

    Many Changes
    1. Added keyboard functionality to drawing app.
    
    2. Added a help page.
    
    3. Moved CSS file into style tags.
    
    4. If user moves mouse outside of canvas, drawing is disabled until they mousedown again. This prevents the canvas from drawing when the user isn't holding their mouse down.

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const eraserSize = document.getElementById('eraserSizeInput');
 
 let nowPainting = false;
 
+// Makes the background white.
+// Prevents background from being transparent on user download.
+clear_canvas()
+
+// Makes user able to draw on the canvas. 
 canvas.addEventListener('mousedown', ()=>{
   nowPainting = true;
 	ctx.beginPath();
@@ -17,6 +22,7 @@ canvas.addEventListener('mousedown', ()=>{
 	ctx.lineCap = 'round';
 })
 
+// Makes the user actually draw stuff on the canvas. 
 canvas.addEventListener('mousemove', (e)=>{
   if (nowPainting === true) {
 		switch (document.querySelector('input:checked').id) {
@@ -24,29 +30,91 @@ canvas.addEventListener('mousemove', (e)=>{
 			case "brush":
         ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
-				break;
+			break;
 
 			case "pencil":
 				ctx.lineWidth = 1;
 				ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
-				break;
-
+			break;
+		
 			case "eraser":
 				ctx.lineWidth = eraserSize.value;
 				ctx.strokeStyle = 'white';
 				ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
-				break;
-
+			break;
 		}
 	}
 })
 
+// Makes user unable to paint if the mouse strays away from the canvas. 
+canvas.addEventListener('mouseout', ()=> {
+	nowPainting = false;
+})
+
+// Stops drawing on the canvas when user releases their mouse. 
 canvas.addEventListener('mouseup', ()=>{
   nowPainting = false;
 })
 
+// Clears the canvas. 
 function clear_canvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#fff";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
+
+// Saves whatever the user drew on the canvas as a png image.
+function save_image() {
+	const a = document.createElement('a');
+	a.download = "canvas_drawing.png";
+	a.href = canvas.toDataURL('png');
+	a.click();
+}
+
+// Adds keyboard functionality to the drawing app.
+document.body.addEventListener('keydown', (e)=> {
+	console.log(e)
+	switch (e.key) {
+
+		case "[":
+		case "-":
+			if (brushSize.value>1) {
+				brushSize.value--;
+			}
+			if (eraserSize.value>1) {
+				eraserSize.value--;
+			}
+		break;
+
+		case "]":
+		case "+":
+			brushSize.value++;
+			eraserSize.value++;
+		break;
+
+		case "b":
+		case "B":
+			document.getElementById('brush').click();
+		break;
+
+		case "p":
+		case "P":
+			document.getElementById('pencil').click();
+		break;
+
+		case "e":
+		case "E":
+			document.getElementById('eraser').click();
+		break;
+
+		case "Delete":
+			clear_canvas()
+		break;
+
+		case "c":
+		case "C":
+			colorPicker.click();
+		break;
+	}
+})
